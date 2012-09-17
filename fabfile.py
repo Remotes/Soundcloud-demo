4c697cfb9230ac30fabf3feb0524f0b81af28bfd
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2012-09-17 02:10:03 -0700

    moved build into a separate function

--- fabfile.py
+++ fabfile.py
@@ -4,13 +4,16 @@ import os
 DROPBOX_DESTINATION_FOLDER = "~/Dropbox/Public/www/"
 
 
+def build():
+	print "Building the project ..."
+
+	local("node ./scripts/build/r.js -o ./scripts/build/Application.build.js")
+	
 def deploy():
 	exclude_list = [".git", ".gitignore", "fabfile.py", "fabfile.pyc", ".DS_Store"] 
 	exclude_string = "--exclude '%s'" % "' --exclude '".join(exclude_list)
 	
-	print "Building the project ..."
-
-	local("node ./scripts/build/r.js -o ./scripts/build/Application.build.js")
+	build()
 
 	print "Deploying to %s" % DROPBOX_DESTINATION_FOLDER
 
